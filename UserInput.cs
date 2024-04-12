0bd47cb0db658ecb04ad17995d937acceaf7762d
sourcesavant <johannes.orth@gmail.com>
2024-04-12 20:44:16 +0200

    Added UserInput class

---
+++ UserInput.cs
@@ -0,0 +1,33 @@
+﻿namespace TheFinalBattle;
+
+public class UserInput
+{
+    Renderer _renderer;
+    
+    public UserInput(Renderer renderer)
+    {
+        _renderer = renderer;
+    }
+
+
+    public string GetTrueProgammerName()
+    {
+        return AskForString("Enter the name of the True Programmer ");
+    }
+
+    private void Prompt(string prompt)
+    {
+        _renderer.Print(prompt);
+    }
+
+    private string AskForString(string prompt)
+    {
+        while (true)
+        {
+            Prompt(prompt);
+            string? input = Console.ReadLine();
+            if (input != null)
+                return input;
+        }
+    }
+}
